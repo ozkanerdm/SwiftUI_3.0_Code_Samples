5600fce52faca6477f55a25e02f8057d3f8c9fb1
Ozkan ERDEM <ozkan.erdm@gmail.com>
2021-12-09 18:41:39 +0300

    Updated KeyboardView sample.

--- KeyboardView.swift
+++ KeyboardView.swift
@@ -25,6 +25,7 @@ struct KeyboardView: View {
                                     }
                                 }
                             }
+                            .submitLabel(.search)
                     }
         }
 }
