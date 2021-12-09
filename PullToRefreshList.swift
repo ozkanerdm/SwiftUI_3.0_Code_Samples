b65d65b4a16e91a19813c8867ee058b008f18d38
Ozkan ERDEM <ozkan.erdm@gmail.com>
2021-12-09 12:53:30 +0300

    Added Pull to refresh List sample.

---
+++ PullToRefreshList.swift
@@ -0,0 +1,38 @@
+//
+//  PullToRefreshList.swift
+//  MediumApp
+//
+//  Created by Ozkan Erdem on 9.12.2021.
+//
+
+import SwiftUI
+
+struct Cars: Identifiable {
+    var id = UUID()
+    var name : String
+}
+
+struct PullToRefreshList: View {
+    @State private var cars:[Cars] = [ Cars(name: "Volvo"),Cars(name: "BMW"),Cars(name: "Mercedes"),Cars(name: "Audi"),Cars(name: "Tesla")]
+
+    var body: some View {
+        NavigationView {
+            List($cars){ $car in
+                VStack(alignment: .leading) {
+                    Text(car.name)
+                        .font(.headline)
+                }
+            }
+            .refreshable {
+                self.cars.append(Cars(name: "Nem Car Model Added!"))
+            }
+            .navigationTitle("Car Models")
+        }
+    }
+}
+
+struct PullToRefreshList_Previews: PreviewProvider {
+    static var previews: some View {
+        PullToRefreshList()
+    }
+}
